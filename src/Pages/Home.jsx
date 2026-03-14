b5142686338c40573313053583ec2c4e649652b3
Md Rifat Bin Sumon <mdsumonpixel@gmail.com>
2026-03-14 17:26:21 +0600

    add HowItWorks steps and professional Footer section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import FAQ from "../Components/FAQ";
+import HowItWorks from "../Components/HowItWorks";
 import Banner from "./Banner";
 import Features from "./Features ";
 
@@ -7,6 +8,7 @@ const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <HowItWorks></HowItWorks>
             <Features></Features>
             <FAQ></FAQ>
         </div>
